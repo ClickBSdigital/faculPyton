038826436dfe72613e6dd12fa12545e569f941c6
Eliandro Silva <clickbsdigital@gmail.com>
2025-03-19 21:51:17 -0400

    aulas de lista faculdade
    Python

---
+++ Lista/concatenacao.py
@@ -0,0 +1,7 @@
+frutas = ["maça","laranja","banana","cereja"]
+print([1,2] + [3,4])
+print(frutas + [3,4])
+
+print(["teste"] * 4)
+print([1,2,["Ola","Adeus"]] * 2)
+
---
+++ Lista/inserir.py
@@ -0,0 +1,8 @@
+uma_lista= ['a','d','f']
+uma_lista[1:1] = ['b','c']
+print(uma_lista)
+# print(len(uma_lista))
+
+uma_lista[4:4] = ['e']
+print(uma_lista)
+print(len(uma_lista))
